57b5dba737c32f4b23b2b85e99a5d05cc4fbb0ce
Mikebainbain <mlopez749@gmail.com>
2022-07-15 19:45:59 -0500

    Se creo la conexion a la Bd de sqlite , se creaarpn las tablas en models.py

--- gestionPedidos/models.py
+++ gestionPedidos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class clientes(models.Model):
+    
+    nombre = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=50)
+    email = models.EmailField()
+    telefono = models.CharField(max_length=10)
+
+class Almacen(models.Model):
+    nombre = models.CharField(max_length=60)
+    lugar = models.IntegerField()
+
+class Pedidos(models.Model):
+    folio = models.IntegerField()
+    fecha = models.DateField()
+    entrregado = models.BooleanField()
